db0366a5ef899579318914e577e9cc1b7737fa49
Takenori Takaki <takenory@gmail.com>
2025-09-08 16:58:04 +0900

    Use explicit index name string for unique_by

--- app/models/issue_card_position.rb
+++ app/models/issue_card_position.rb
@@ -5,7 +5,7 @@ class IssueCardPosition < ActiveRecord::Base
       ordered_issue_positions = ordered_issue_ids.each_with_index.map { |id, i| { issue_id: id, position: i } }
       opts = { update_only: %i[position] }
       # :unique_by option is supported only by PostgreSQL and SQLite3
-      opts[:unique_by] = :index_issue_card_positions_on_issue_id if Redmine::Database.postgresql? || Redmine::Database.sqlite?
+      opts[:unique_by] = 'index_issue_card_positions_on_issue_id' if Redmine::Database.postgresql? || Redmine::Database.sqlite?
       self.upsert_all(ordered_issue_positions, **opts)
     end
   end
